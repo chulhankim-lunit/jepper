a9041855bdf13fc8db07dab279fbc1d3410db6c6
Chulhan Kim <chulhan.kim@lunit.io>
2025-04-30 12:47:49 +0900

    ci: add a deployment script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+args=($@)
+token=${args[0]}
+submodules=$(git submodule status)
+
+while read -r line || [[ -n $line ]]; do
+    name=$(awk -F '[ ]' '{print $2}' <<< $line)
+    version=$(awk -F '[()]' '{print $2}' <<< $line)
+    echo "Deploying $name:$version..."
+    curl -L \
+        -X POST \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer ${token}" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        https://api.github.com/repos/chulhankim-lunit/$name/dispatches \
+        -d "{\"event_type\":\"deploy\",\"client_payload\":{\"ref\":\"$version\"}}"
+done <<< "$submodules"
